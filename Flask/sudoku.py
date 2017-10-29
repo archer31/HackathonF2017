ca7b019a32bbbf70461e124fae3d20c8f9a7b364
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-10-28 18:20:39 -0700

    create template

--- Flask/sudoku.py
+++ Flask/sudoku.py
@@ -1,11 +1,15 @@
-from flask import Flask
+import subprocess
+from flask import Flask, render_template
+from get_puzzle import get_puzzle
+
 app = Flask(__name__)
 
 @app.route('/')
 def web_page():
   author = "Jake, Ryan, and Xandy"
   name = "AI Love Sudoku"
-  return render_template('index.html', author=author, name=name)
+  puzzle = get_puzzle()
+  return render_template('index.html', puzzle=puzzle)
 
 if __name__ == '__main__':
   app.run()
