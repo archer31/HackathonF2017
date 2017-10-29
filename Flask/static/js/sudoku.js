af7e704a0827c68ffeec88284a036d52c17182b6
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-10-28 19:31:30 -0700

    wip

--- Flask/static/js/sudoku.js
+++ Flask/static/js/sudoku.js
@@ -1,4 +1,4 @@
-function createPlayerGrid(puzzleArray)
+function createPlayerGrid(puzzleStr)
 {
   var iter=0;
   for(var i=1; i<=9; i++)
@@ -10,15 +10,21 @@ function createPlayerGrid(puzzleArray)
     }
   }
 }
-function createAIGrid(puzzleArray)
+function createAIGrid(puzzleStr)
 {
+  puzzleArray = puzzleStr.split("")
   var iter=0;
   for(var i=1; i<=9; i++)
   {
-    for(var j=1; j<=9; j++)
+    for(var j=1; j<=9; j++, iter++)
     {
-      document.getElementById(("1"+i)+j).innerHTML = "1";
-      iter++;
+      var id = ("1"+i)+j;
+      var val = puzzleArray[iter];
+      if(val == "0") {
+        document.getElementById(id).innerHTML = '-';
+      } else {
+        document.getElementById(id).innerHTML = val;
+      }
     }
   }
 }
